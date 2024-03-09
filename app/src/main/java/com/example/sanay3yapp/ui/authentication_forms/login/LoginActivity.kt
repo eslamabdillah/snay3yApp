623399d35d505671059f110766b67ebf6493ac25
mahmoudmagdy198 <mahmoud.magdy11198@gmail.com>
2024-03-09 16:53:26 +0200

    handle button signup in LoginActivity to go to choose_login_signup activity

--- app/src/main/java/com/example/sanay3yapp/ui/authentication_forms/login/LoginActivity.kt
+++ app/src/main/java/com/example/sanay3yapp/ui/authentication_forms/login/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.example.sanay3yapp.ui.authentication_forms.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import com.example.sanay3yapp.R
 import com.example.sanay3yapp.databinding.ActivityLoginBinding
+import com.example.sanay3yapp.ui.authentication_forms.choose_login_signup.Choose_login_signupActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,6 +17,11 @@ class LoginActivity : AppCompatActivity() {
         loginBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(loginBinding.root)
 
+        // go to choose_login_signup activity
+        val intent = Intent(this,Choose_login_signupActivity::class.java)
+        loginBinding.buttonSignup.setOnClickListener {
+            startActivity(intent)
+        }
 
 
     }
